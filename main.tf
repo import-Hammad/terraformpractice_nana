e4e0004e84412c68fd7f72915249a7d2669e3de2
import-hammad <hammadmalik67000@gmail.com>
2026-06-15 23:26:41 +0500

    ok

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ variable "vpc_cidr_bock"{}
 variable "subnet_cidr_block"{}
 variable "avalability_zone"{}
 variable "env_prefix"{}
+variable "myip" {}
+variable "instance_type" {}
 
 
 resource "aws_vpc" "my_app_vpc"{
@@ -34,7 +36,7 @@ resource "aws_internet_gateway" "myapp_igw"{
         Name = "${var.env_prefix}-igw"
     }
 }
-
+/*
 resource "aws_route_table" "myapp_route_table"{
     vpc_id = aws_vpc.my_app_vpc.id
     route {
@@ -43,6 +45,74 @@ resource "aws_route_table" "myapp_route_table"{
     }
     tags = {
         Name = "${var.env_prefix}-rtb"
+    }
+}
+
+
+resource "aws_route_table_association" "a-rtb_subnet" {
+    subnet_id = aws_subnet.my_dev_subnet_1.id
+    route_table_id = aws_route_table.myapp_route_table.id
+
+}
+*/
+
+resource "aws_default_route_table" "main-rtb"{
+    default_route_table_id = aws_vpc.my_app_vpc.default_route_table_id
+     route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp_igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}main-rtb"
+    }
+}
+
+
+resource "aws_default_security_group" "default-sg"{
+    
+    vpc_id = aws_vpc.my_app_vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = [var.myip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+
+    }
+    tags = {
+        Name = "${var.env_prefix}default-sg"
+    }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_instance" "myapp-server"{
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    availability_zone = var.avalability_zone
+    subnet_id = aws_subnet.my_dev_subnet_1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    associate_public_ip_address = true
+    key_name = file("ansible-master-key.pem")
+
+    tags = {
+        Name: "${var.env_prefix}-server"
     }
 }
 
