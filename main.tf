2ddff49310490dea618737b8400c95239527bae1
import-hammad <hammadmalik67000@gmail.com>
2026-06-18 23:23:58 +0500

    ok

--- main.tf
+++ main.tf
@@ -24,6 +24,20 @@ module "myapp-subnet" {
 }
 
 
+module "myapp-server" {
+    source = "./modules/webserver"
+    vpc_id = aws_vpc.my_app_vpc.id
+    my_ip = var.myip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    subnet_id = module.myapp-subnet.subnet.id
+    
+
+}
+
+
 /*
 resource "aws_route_table" "myapp_route_table"{
     vpc_id = aws_vpc.my_app_vpc.id
@@ -47,73 +61,7 @@ resource "aws_route_table_association" "a-rtb_subnet" {
 
 
 
-resource "aws_default_security_group" "default-sg"{
-    
-    vpc_id = aws_vpc.my_app_vpc.id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.myip]
-    }
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-
-    }
-    tags = {
-        Name = "${var.env_prefix}-default-sg"
-    }
-}
-
-data "aws_ami" "latest-amazon-linux-image"{
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-
-
-resource "aws_key_pair" "ansible-key" {
-    key_name = "ansible-key"
-    public_key = file(var.public_key_location)
-}
-
-
-resource "aws_instance" "myapp-server" {
-    ami = data.aws_ami.latest-amazon-linux-image.id
-    instance_type = var.instance_type
-    
-    subnet_id = module.myapp-subnet.subnet.id
-    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ansible-key.key_name
-
-    user_data = file("entry-script.sh")
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "${var.env_prefix}-server"
-    }
-}
 
 
 
---
+++ modules/webserver/main.tf
@@ -0,0 +1,75 @@
+resource "aws_default_security_group" "default-sg"{
+    
+    vpc_id = var.vpc_id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+
+    }
+    tags = {
+        Name = "${var.env_prefix}-default-sg"
+    }
+}
+
+data "aws_ami" "latest-amazon-linux-image"{
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = [var.image_name]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+
+
+resource "aws_key_pair" "ansible-key" {
+    key_name = "ansible-key"
+    public_key = file(var.public_key_location)
+}
+
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ansible-key.key_name
+
+    user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y docker
+                systemctl start docker
+                systemctl enable docker
+                usermod -aG docker ec2-user
+                docker run -d -p 8080:80 nginx
+                EOF
+    user_data_replace_on_change = true
+
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
+}
